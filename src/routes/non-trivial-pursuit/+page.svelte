199cbeef5449453fb94ffe9f67d48fef990256fe
wise-bit <satrajit2016@gmail.com>
2025-04-01 03:05:25 -0400

    wip

--- src/routes/non-trivial-pursuit/+page.svelte
+++ src/routes/non-trivial-pursuit/+page.svelte
@@ -3,6 +3,7 @@
   import { onMount } from 'svelte';
 
   interface Question {
+    index: number;
     points: string;
     question: string;
     answer: string;
@@ -23,7 +24,12 @@
   let categories: string[] = [];
   let questions: Question[][] = [];
 
+  let currentFace = 1;
+  let rolling = false;
+  let rollInterval: string | number | NodeJS.Timeout | undefined;
+
   const defaultQuestion: Question = {
+    index: 0,
     points: '',
     question: '',
     answer: '',
@@ -33,7 +39,6 @@
 
   const selectedQuestion = writable(defaultQuestion);
   const showModal = writable(false);
-  const showFinalJeopardy = writable(false);
 
   const handleFileUpload = async (event: Event) => {
     const input = event.target as HTMLInputElement;
@@ -51,15 +56,18 @@
           (categoryObject) => Object.keys(categoryObject)[0],
         );
 
-        questions = data[boardTitle].map((categoryObject) => {
+        questions = data[boardTitle].map((categoryObject, categoryIndex) => {
           const categoryName = Object.keys(categoryObject)[0];
-          return categoryObject[categoryName].map((question) => ({
-            points: question.points,
-            question: question.question,
-            answer: question.answer,
-            visited: false,
-            showAnswer: false,
-          }));
+          return categoryObject[categoryName].map(
+            (question, questionIndex) => ({
+              index: categoryIndex * 100 + questionIndex + 1,
+              points: question.points,
+              question: question.question,
+              answer: question.answer,
+              visited: false,
+              showAnswer: false,
+            }),
+          );
         });
       };
 
@@ -70,6 +78,9 @@
   const selectQuestion = (question: Question, i: number, j: number) => {
     selectedQuestion.set(question);
     questions[i][j].visited = true;
+  };
+
+  const openModal = () => {
     showModal.set(true);
   };
 
@@ -87,7 +98,7 @@
 
   function downloadSample() {
     const a = document.createElement('a');
-    a.href = 'samples/trials-jeopardy-sample.json';
+    a.href = 'samples/trials-pursuit-sample.json';
     a.download = 'sample.json';
 
     document.body.appendChild(a);
@@ -97,15 +108,10 @@
 
   // dice stuff
 
-  let currentFace = 1;
-  let rolling = false;
-  let rollInterval: string | number | NodeJS.Timeout | undefined;
-
   const rollDice = () => {
     if (rolling) return;
 
     rolling = true;
-
     rollInterval = setInterval(() => {
       currentFace = Math.floor(Math.random() * 6) + 1;
     }, 100);
@@ -149,7 +155,7 @@
     );
 </script>
 
-<button class="back-button" on:click={() => goBack()}>{'< back'}</button>
+<button class="back-button hov-btn" on:click={() => goBack()}>{'< back'}</button>
 
 {#if false}
   <div class="container">
@@ -178,7 +184,7 @@
       <div class="dice-container">
         <h1>{boardTitle}</h1>
         <div
-          class="dice"
+          class="dice hov-btn"
           aria-label="Roll the dice"
           role="button"
           tabindex="0"
@@ -193,6 +199,12 @@
           {:else}
             Rolled a {currentFace} !
           {/if}
+        </div>
+
+        <div>
+          <button class="show-question-btn hov-btn" on:click={openModal}>
+            show question!
+          </button>
         </div>
       </div>
 
@@ -211,6 +223,43 @@
     </div>
 
     <div class="score-container">asd</div>
+  </div>
+{/if}
+
+{#if $showModal}
+  <div
+    class="modal-overlay"
+    role="button"
+    tabindex="0"
+    on:click={closeModal}
+    on:keydown={closeModal}
+  >
+    <div
+      class="modal"
+      role="button"
+      tabindex="0"
+      on:click|stopPropagation
+      on:keydown|stopPropagation
+    >
+      {#if $selectedQuestion}
+        <h4>[{$selectedQuestion.points} points]</h4>
+        <br />
+        <h2>{$selectedQuestion.question}</h2>
+        {#if $selectedQuestion.showAnswer}
+          <p>{$selectedQuestion.answer}</p>
+        {/if}
+        <div class="button-set">
+          <button
+            class="reveal-button hov-btn"
+            on:click={() =>
+              ($selectedQuestion.showAnswer = !$selectedQuestion.showAnswer)}
+          >
+            reveal answer
+          </button>
+          <button class="close-button hov-btn" on:click={closeModal}>close</button>
+        </div>
+      {/if}
+    </div>
   </div>
 {/if}
 
@@ -240,8 +289,8 @@
   .back-button {
     font-family: 'Pixelify Sans', 'Comic Sans MS', 'Arial', sans-serif;
     position: absolute;
-    top: 1rem;
-    left: 1rem;
+    top: 2rem;
+    left: 2rem;
     padding: 0.5rem 1rem;
     background-color: #333;
     color: #f9f9f9;
@@ -266,6 +315,10 @@
     width: 70%;
     height: 100%;
     background-color: #000;
+    border-right: 10px solid #0a0a0a;
+  }
+  .board-container > * {
+    border-right: none;
   }
 
   .dice-container {
@@ -290,14 +343,17 @@
     cursor: pointer;
     background-color: #783333;
     box-shadow: 0 4px 6px rgba(0, 0, 0, 0.2);
+  }
+
+  .hov-btn {
     transition: transform 0.2s;
   }
 
-  .dice:hover {
+  .hov-btn:hover {
     transform: scale(1.15);
   }
 
-  .dice:active {
+  .hov-btn:active {
     transform: scale(0.95);
   }
 
@@ -306,22 +362,13 @@
     font-size: 2rem;
   }
 
-  .score-container {
-    flex: 1;
-    flex-basis: 30%;
-    width: 30%;
-    background-color: #242e2b;
-    border-left: 10px solid #101212;
-    padding: 2rem;
-  }
-
   /* board */
 
   .board {
     display: grid;
     grid-template-columns: repeat(5, 10rem);
     grid-template-rows: repeat(5, 10rem);
-    gap: 2px;
+    gap: 1rem;
     margin: 20px auto;
   }
 
@@ -333,7 +380,7 @@
     justify-content: center;
     background-color: #000000;
     border: 2px solid #000000;
-    border-radius: 8px;
+    border-radius: 1rem;
     font-size: 2rem;
   }
 
@@ -341,5 +388,91 @@
     background-color: #a0a0a0;
     color: rgb(0, 0, 0);
     font-weight: bold;
+  }
+
+  .show-question-btn {
+    font-family: 'Pixelify Sans', 'Comic Sans MS', 'Arial', sans-serif;
+    margin-top: 2rem;
+    padding: 0.5rem 1rem;
+    background-color: #64355b;
+    color: #fff;
+    border: 0.2rem solid #391f34;
+    border-radius: 0.3rem;
+    font-size: 1.5rem;
+    cursor: pointer;
+    font-weight: 500;
+    transition: transform 0.2s;
+  }
+
+  /* scores */
+
+  .score-container {
+    flex: 1;
+    flex-basis: 30%;
+    width: 30%;
+    background-color: #242e2b;
+    border-left: 10px solid #101212;
+    padding: 2rem;
+  }
+
+  /* modal */
+
+  .modal-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.8);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .modal {
+    background: #1a1a1a;
+    padding: 2rem;
+    border: 0.3rem solid white;
+    border-radius: 0.3rem;
+    text-align: center;
+    align-items: center;
+    color: #f9f9f9;
+    max-width: 500px;
+    width: 80%;
+    font-size: 2rem;
+  }
+
+  .button-set {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    gap: 2rem;
+  }
+
+  .reveal-button {
+    font-family: 'Pixelify Sans', 'Comic Sans MS', 'Arial', sans-serif;
+    margin-top: 1rem;
+    padding: 0.5rem 1rem;
+    background-color: #465e4b;
+    color: #fff;
+    border: 0.3rem solid #1b291e;
+    border-radius: 8px;
+    font-size: 1.5rem;
+    cursor: pointer;
+    font-weight: 500;
+  }
+
+  .close-button {
+    font-family: 'Pixelify Sans', 'Comic Sans MS', 'Arial', sans-serif;
+    margin-top: 1rem;
+    padding: 0.5rem 1rem;
+    background-color: #845454;
+    color: #fff;
+    border: 0.3rem solid #463131;
+    border-radius: 8px;
+    font-size: 1.5rem;
+    cursor: pointer;
+    font-weight: 500;
   }
 </style>
